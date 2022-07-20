4fa35e28e2adc1187592977ded24dc59df567abc
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-07-20 00:21:37 -0500

    manager column now has the manager's full name

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,13 +11,13 @@ VALUES
   ('Manager', 125000, 3);
 INSERT INTO employees (first_name, last_name, role, manager)
 VALUES
-  ('Ronald', 'Firbank', NULL, 1),
-  ('Virginia', 'Woolf', 1, 1),
+  ('Ronald', 'Firbank', NULL, NULL),
+  ('Virginia', 'Woolf', 1, 10),
   ('Piers', 'Gaveston', 1, 0),
-  ('Charles', 'LeRoi', 2, 1),
-  ('Katherine', 'Mansfield', 2, 1),
+  ('Charles', 'LeRoi', 2, 10),
+  ('Katherine', 'Mansfield', 2, 10),
   ('Dora', 'Carrington', 3, 0),
   ('Edward', 'Bellamy', 3, 0),
-  ('Montague', 'Summers', 3, 1),
-  ('Octavia', 'Butler', 3, 1),
-  ('Unica', 'Zurn', 4, 1);
+  ('Montague', 'Summers', 3, 10),
+  ('Octavia', 'Butler', 3, 10),
+  ('Unica', 'Zurn', 4, 10);
--- server.js
+++ server.js
@@ -9,21 +9,3 @@ db.connect(err => {
     // Call function to initialize app
     promptUser();
 });
-
-
-// db.query(`SELECT * FROM departments`, (err, rows) => {
-//     console.table(rows);
-//     console.log(rows);
-// });
-
-// db.query(`SELECT roles.id, roles.name, roles.salary, departments.name AS department_name FROM roles INNER JOIN departments ON roles.department_id = departments.id;`, (err, rows) => {
-//     console.table(rows);
-// });
-
-// db.query(`SELECT roles.*, departments.name AS department_name FROM roles INNER JOIN departments ON roles.department_id = departments.id;`, (err, rows) => {
-//     console.table(rows);
-// });
-
-// db.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.name AS role_name, employees.manager_id FROM employees INNER JOIN roles ON employees.role_id = roles.id;`, (err, rows) => {
-//     console.table(rows);
-// });
